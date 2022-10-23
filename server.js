398f92d09416dc1b36abcc6ef983ebfec14f3b7c
sushant102004 <sushant.dhiman9812@gmail.com>
2022-10-23 18:29:53 +0530

    Tour Model Created

--- server.js
+++ server.js
@@ -11,6 +11,34 @@ mongoose.connect(DB, {
     useNewUrlParser: true,
 }).then(() => console.log('Connected to DB'));
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        default: 0
+    }
+})
+
+const tourModel = new mongoose.model('Tour', tourSchema);
+
+const newTour = new tourModel({
+        name : 'My Tour',
+        price: 344,
+        rating: 4
+    }
+);
+
+newTour.save().then(() => console.log(newTour));
+
+
+
 app.listen(PORT, () => {
     console.log('Listening on post: ' + process.env.PORT);
 })
