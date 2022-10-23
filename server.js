b01ff6c0a8c20ec20e42c8fd7677e8d15d697727
sushant102004 <sushant.dhiman9812@gmail.com>
2022-10-23 17:55:33 +0530

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
+
+const DB = process.env.DB
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+}).then(() => console.log('Connected to DB'));
 
 app.listen(PORT, () => {
     console.log('Listening on post: ' + process.env.PORT);
