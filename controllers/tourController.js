18cf19bfbf3ec3181ff7be93ab390db41dbc25ab
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-11 18:28:24 +0530

    Half Refactoring Completed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,13 +15,15 @@ class APIFeatures {
     }
 
     filter(){
-        const queryObj = {...this.query};
-        const fieldsToExclude = ['page', 'sort', 'limit', 'fields'];
-        fieldsToExclude.forEach(el => delete queryObj[el])
+        let queryObj = { ...this.queryString };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
 
-        // Advance Filtering
+        excludeFields.forEach(el => delete queryObj[el])
+
+        // Filtering
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
 
         this.query.find(JSON.parse(queryStr))
         return this
@@ -31,45 +33,55 @@ class APIFeatures {
         if(this.queryString.sort){
             const sortBy = this.queryString.sort.split(',').join(' ')
             this.query = this.query.sort(sortBy)
-        } else {
-            this.query = this.query.sort('-createdAt')
-        }
+        } else this.query = this.query.sort('-createdAt')
+        
         return this
     }
 
-    limitFields(){
+    limit(){
         if(this.queryString.fields){
             const fields = this.queryString.fields.split(',').join(' ')
             this.query = this.query.select(fields)
-        } else this.query = this.query.select('-__v')
-        return this
-    }
+        } else this.query = this.query.select('-__V')
 
-    pagination(){
-        const page = this.queryString.page * 1 || 1;
-        const limit = this.queryString.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        this.query = this.query.skip(skip).limit(limit)
         return this
     }
 }
 
 exports.getAllTours = async (req, res) => {
     try {
-        const queryObj = { ...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        // let queryObj = { ...req.query };
+        // const excludeFields = ['page', 'sort', 'limit', 'fields'];
 
-        excludeFields.forEach(el => delete queryObj[el])
+        // excludeFields.forEach(el => delete queryObj[el])
 
-        // Filtering
-        const queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-        const query = Tour.find(queryStr)
+        // // Filtering
+        // let queryStr = JSON.stringify(queryObj)
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
 
-        const tours = await query;
+        // let query = Tour.find(JSON.parse(queryStr))
+
+        // Sorting
+        
+        // if(this.queryString){
+        //     const sortBy = req.query.sort.split(',').join(' ')
+        //     query = this.query.sort(sortBy)
+        // } else query = this.query.sort('-createdAt')
+
+        // Limiting
+        // if(req.query.fields){
+        //     const fields = req.query.fields.split(',').join(' ')
+        //     query = query.select(fields)
+        // } else query = query.select('-name')
+        
+
+
+        // const tours = await query;
+
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limit()
+        const tours = await features.query
+
         res.status(200).json({
             status: 'success',
             data: {
@@ -79,10 +91,11 @@ exports.getAllTours = async (req, res) => {
     } catch (err) {
         res.status(404).json({
             status: 'fail',
-            message: err
+            message: err,
         })
     }
 }
+
 
 exports.addNewTour = async (req, res) => {
     try {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const tourController = require('../controllers/tourController')
 
 
-router.route('/top-five-tours').get(tourController.getTopFiveTours, tourController.getAllTours)
+// router.route('/top-five-tours').get(tourController.getTopFiveTours, tourController.getAllTours)
 
 router.route('/').get(tourController.getAllTours).post(tourController.addNewTour)
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour)
