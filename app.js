d09c2bb92f14a607b1a2f0c0bc36fbe3a585f09f
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-21 19:40:31 +0530

    Global Error Handling Middleware Implemented

--- app.js
+++ app.js
@@ -14,10 +14,27 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res) => {
-    res.status(404).json({
-        status: 'not found',
-        message: `The route ${req.originalUrl} not found`
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'not found',
+    //     message: `The route ${req.originalUrl} not found.`
+    // })
+
+    const err = new Error(`The route ${req.originalUrl} was not found`)
+    err.statusCode = 404
+    err.status = 'not found'
+
+    next(err)
+})
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    err.status = err.status || 'fail'
+    err.statusCode = err.statusCode || 500
+
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message
     })
 })
 
